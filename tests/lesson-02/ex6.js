e0ad6f8f1cdc32da06b97c9b1a1f9438f7b6a383
Ngoc Hoan <raijinnguyen1302@gmail.com>
2026-07-18 19:18:46 +0700

    feat: 🟢 🚀 update bai tap js loop

---
+++ tests/lesson-02/ex6.js
@@ -0,0 +1,11 @@
+for (let i = 12; i <= 30; i++) {
+  console.log(`Các số trong vòng lặp ${i}`)
+}
+/*------------------------------------------------------------------------------------------*/
+// const printNumbers = () => {
+//   for (let i = 12; i <= 30; i++) {
+//     console.log(i)
+//   }
+// }
+
+// printNumbers()
---
+++ tests/lesson-02/ex7.js
@@ -0,0 +1,17 @@
+for (let i = 1000; i <= 2000; i++) {
+  if (i % 3 === 0) {
+    console.log(`Các số chia hết cho 3 ${i}`)
+  }
+}
+
+/*------------------------------------------------------------------------------------------*/
+
+// const printDivideByThree = () => {
+//   for (let i = 1000; i <= 2000; i++) {
+//     if (i % 3 === 0) {
+//       console.log(`Các số chia hết cho 3 ${i}`)
+//     }
+//   }
+// }
+
+// printDivideByThree()
